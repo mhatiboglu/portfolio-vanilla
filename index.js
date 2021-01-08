476458f7d2b2c771723101b53d9aedab534b46af
mhatiboglu <mh1741mh@gmail.com>
2021-01-08 12:07:16 +0000

    Create dark/light mode

--- index.js
+++ index.js
@@ -12,3 +12,42 @@ hamburger.addEventListener("click", () => {
   line2.classList.toggle("passive");
   line3.classList.toggle("rotateleft");
 });
+
+//Light/ Dark Mode
+
+// Select the related part
+const btn = document.querySelector(".btn");
+const btnImg = document.querySelector(".button_image");
+const landingHeader = document.querySelector(".landing_name");
+const socialMeadiaSvgs = document.querySelectorAll(".social_media_svg");
+const navTextLink = document.querySelectorAll(".textLink");
+const contact_link = document.querySelectorAll(".contact_link");
+
+// Handle multiple nodes
+
+const toggleMultipleQuerry = (nodes) => {
+  const nodesArray = Array.from(nodes);
+  nodesArray.map((item) => {
+    item.classList.toggle("dark-theme");
+  });
+};
+
+// Handle click Darl/Light button
+
+btn.addEventListener("click", function() {
+  // Add/Remove  .dark-theme class to related elements.
+  document.body.classList.toggle("dark-theme");
+  landingHeader.classList.toggle("dark-theme");
+  toggleMultipleQuerry(socialMeadiaSvgs);
+  toggleMultipleQuerry(contact_link);
+  toggleMultipleQuerry(navTextLink);
+
+  //   for (var item of socialMeadiaSvgs) {
+  //     item.classList.toggle("dark-theme");
+  //   }
+
+  btnImg.src =
+    btnImg.src == "https://i.ibb.co/7JfqXxB/sunny.png"
+      ? "https://i.ibb.co/FxzBYR9/night.png"
+      : "https://i.ibb.co/7JfqXxB/sunny.png";
+});
